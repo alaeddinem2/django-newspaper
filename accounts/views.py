e39b1fa98c13644be930c3c859620d8425e75c6b
unknown <telliala@gmail.com>
2023-10-11 07:36:26 +0100

    add login logout change passwd ..etc

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
 from .forms import CustomUserCreationForm
+
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
